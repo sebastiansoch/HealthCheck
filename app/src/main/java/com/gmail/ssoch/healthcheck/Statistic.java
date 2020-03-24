7752f197e8e6bdf6b85be89c8f8003aec0fd5414
Sebastian Soch <sebastian.soch@gmail.com>
2020-03-24 22:49:05 +0100

    Create Statistic View

--- app/src/main/java/com/gmail/ssoch/healthcheck/Statistic.java
+++ app/src/main/java/com/gmail/ssoch/healthcheck/Statistic.java
@@ -1,14 +1,23 @@
 package com.gmail.ssoch.healthcheck;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
 
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.android.material.tabs.TabLayout;
+
+
 public class Statistic extends AppCompatActivity {
+
+    private TabLayout tabLayout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_statistic);
+
+        tabLayout = findViewById(R.id.statistic_tabs);
+
     }
+
 }
