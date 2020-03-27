9794e9d81c8e5739a7c676dbb29ecdb633d7ccc6
Sebastian Soch <sebastian.soch@gmail.com>
2020-03-27 20:20:45 +0100

    Flags added to the languages

--- app/src/main/java/com/gmail/ssoch/healthcheck/Settings.java
+++ app/src/main/java/com/gmail/ssoch/healthcheck/Settings.java
@@ -3,10 +3,15 @@ package com.gmail.ssoch.healthcheck;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.res.Configuration;
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.ImageButton;
+import android.widget.ListAdapter;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import androidx.appcompat.app.AlertDialog;
@@ -19,48 +24,17 @@ import static android.widget.Toast.LENGTH_SHORT;
 public class Settings extends AppCompatActivity {
     private Locale myLocale;
     private String currentLanguage = "en", currentLang;
-
-
+    private AlertDialogItem[] alertDialogItems = new AlertDialogItem[2];
+    private ListAdapter alertDialogListAdapter;
     private ImageButton languageBtn;
-    private ImageButton.OnClickListener languageBtnListener = new View.OnClickListener() {
+    private ImageButton.OnClickListener languageBtnListener;
+    private ImageButton syncBtn;
+    private Button cancelBtn;
+    private Button.OnClickListener cancelBtnListener = new View.OnClickListener() {
         @Override
         public void onClick(View v) {
-            final CharSequence[] language = {"English", "Polski"};
-            final String[] chosenLanguage = {"en"};
-
-            AlertDialog.Builder builder = new AlertDialog.Builder(Settings.this);
-            builder.setTitle("Choose language ...");
-
-            builder.setSingleChoiceItems(language, -1, new DialogInterface.OnClickListener() {
-                @Override
-                public void onClick(DialogInterface dialog, int which) {
-                    switch (which) {
-                        case 0:
-                            chosenLanguage[0] = "en";
-                            break;
-                        case 1:
-                            chosenLanguage[0] = "pl";
-                            break;
-                    }
-                }
-            });
-
-            builder.setPositiveButton(R.string.alter_dialog_ok, new DialogInterface.OnClickListener() {
-                @Override
-                public void onClick(DialogInterface dialog, int which) {
-                    setLocale(chosenLanguage[0]);
-                }
-            });
-
-            builder.setNegativeButton(R.string.alter_dialog_cancel, new DialogInterface.OnClickListener() {
-                @Override
-                public void onClick(DialogInterface dialog, int which) {
-
-                }
-            });
-
-            AlertDialog dialog = builder.create();
-            dialog.show();
+            Intent intent = new Intent(Settings.this, MainActivity.class);
+            startActivity(intent);
         }
     };
 
@@ -80,21 +54,92 @@ public class Settings extends AppCompatActivity {
         }
     }
 
-
-    private ImageButton syncBtn;
-    private Button cancelBtn;
-    private Button.OnClickListener cancelBtnListener = new View.OnClickListener() {
-        @Override
-        public void onClick(View v) {
-            Intent intent = new Intent(Settings.this, MainActivity.class);
-            startActivity(intent);
-        }
-    };
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_settings);
+
+        alertDialogItems[0] = new AlertDialogItem("English", getResources().getDrawable(R.drawable.ic_flag_uk, null));
+        alertDialogItems[1] = new AlertDialogItem("Polski", getResources().getDrawable(R.drawable.ic_flag_pl, null));
+
+        alertDialogListAdapter = new ArrayAdapter<AlertDialogItem>(
+                this,
+                android.R.layout.select_dialog_singlechoice,
+                android.R.id.text1,
+                alertDialogItems) {
+
+            public View getView(int position, View convertView, ViewGroup parent) {
+                View view = super.getView(position, convertView, parent);
+
+                TextView textView = view.findViewById(android.R.id.text1);
+                textView.setText(alertDialogItems[position].language);
+                textView.setCompoundDrawablesWithIntrinsicBounds(alertDialogItems[position].icon, null, null, null);
+
+                int marginIconAndText = (int) (5 * getResources().getDisplayMetrics().density + 0.5f);
+
+                textView.setCompoundDrawablePadding(marginIconAndText);
+
+                return view;
+            }
+        };
+
+        languageBtnListener = new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                final CharSequence[] language = {"English", "Polski"};
+                final String[] chosenLanguage = {"en"};
+
+                AlertDialog.Builder builder = new AlertDialog.Builder(Settings.this);
+                builder.setTitle("Choose language ...");
+
+                builder.setAdapter(alertDialogListAdapter, new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        switch (which) {
+                            case 0:
+//                                chosenLanguage[0] = "en";
+                                setLocale("en");
+                                break;
+                            case 1:
+//                                chosenLanguage[0] = "pl";
+                                setLocale("pl");
+                                break;
+                        }
+                    }
+                });
+
+//            builder.setSingleChoiceItems(language, -1, new DialogInterface.OnClickListener() {
+//                @Override
+//                public void onClick(DialogInterface dialog, int which) {
+//                    switch (which) {
+//                        case 0:
+//                            chosenLanguage[0] = "en";
+//                            break;
+//                        case 1:
+//                            chosenLanguage[0] = "pl";
+//                            break;
+//                    }
+//                }
+//            });
+
+                builder.setPositiveButton(R.string.alter_dialog_ok, new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        setLocale(chosenLanguage[0]);
+                    }
+                });
+
+                builder.setNegativeButton(R.string.alter_dialog_cancel, new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+
+                    }
+                });
+
+                AlertDialog dialog = builder.create();
+                dialog.show();
+            }
+        };
 
         currentLanguage = getIntent().getStringExtra(currentLang);
 
@@ -105,6 +150,18 @@ public class Settings extends AppCompatActivity {
 
         cancelBtn = findViewById(R.id.settings_cancel_Btn);
         cancelBtn.setOnClickListener(cancelBtnListener);
+
+
+    }
+
+    private class AlertDialogItem {
+        final String language;
+        final Drawable icon;
+
+        AlertDialogItem(String language, Drawable icon) {
+            this.language = language;
+            this.icon = icon;
+        }
     }
 
 }
